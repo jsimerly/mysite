47d2de0d05868bf5537109a8aa712dde440b2545
Jacob <simerly81@gmail.com>
2021-12-02 21:34:56 -0500

    New Navbar

--- main/static/js/script.js
+++ main/static/js/script.js
@@ -1,16 +1,16 @@
-function myFunction() {
-    document.getElementById('myDropdown').classList.toggle('show')
+function dropDown() {
+    document.getElementById('account-dropdown').classList.toggle('show')
 }
 
 window.onclick = function(event){
-    if (!event.target.matches('.dropbtn')) {
+    if (!event.target.matches('.account-btn')) {
         var dropdowns = document.getElementsByClassName('dropdown-content');
         var i;
-
+        
         for (i=0; i < dropdowns.length; i++) {
             var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
+            if (openDropdown.classList.contains('show')){
+                openDropdown.classList.remove('show')
             }
         }
     }
